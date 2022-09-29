2673a42d22c6129cddc390d2d2187c33d4482aea
Nayanajith Priyasad <nayanajithpriyasad@gmail.com>
2022-09-29 22:20:49 +0530

    add axios fix

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,75 +1,66 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
-import "./Carousel.css";
+import './Carousel.css'
 import Slide from "../Slide/Slide";
 
-const Carousel = ({ slide_count = 2, infinte = true }) => {
-	const [current, setCurrent] = useState(0);
-	const [slides, setSlides] = useState([]);
-	const [length, setLength] = useState(0);
-	const [nextDisabled, setNextDisabled] = useState(false);
-	const [prevDisabled, setPrevDisabled] = useState(false);
+const Carousel = ({ slide_count=2, infinte=true }) => {
+  const [current, setCurrent] = useState(0);
+  const [slides, setSlides] = useState([]);
+  const [length, setLength] = useState(0)
+  const [nextDisabled, setNextDisabled] = useState(false)
+  const [prevDisabled, setPrevDisabled] = useState(false)
 
-	useEffect(() => {
-		axios
-			.get(`${process.env.BASE_URL}/carousel?count=${slide_count}`)
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data.data);
-				setSlides(data.data);
-				setLength(data.data.length);
-			})
-			.catch((err) => {
-				console.log(err.message);
-			});
-	}, [slide_count]);
+  useEffect(()=> {
+    axios.get(`http://localhost:3600/api/carousel?count=${slide_count}`)
+    .then((response) => response.json())
+    .then((data) => {
+       console.log(data.data);
+       setSlides(data.data);
+       setLength(data.data.length)
+    })
+    .catch((err) => {
+       console.log(err.message);
+    });
+  }, [slide_count])
 
-	const nextSlide = () => {
-		if (infinte) {
-			setCurrent(current === length - 1 ? 0 : current + 1);
-		} else {
-			if (current === length - 1) {
-				setNextDisabled(true);
-			} else {
-				setCurrent(current + 1);
-			}
-		}
-	};
+  const nextSlide = () => {
+    if(infinte){
+      setCurrent(current === length - 1 ? 0 : current + 1);
+    } else{
+      if(current === length - 1){
+        setNextDisabled(true)
+      } else {
+        setCurrent(current + 1)
+      }
+    }
+  };
 
-	const prevSlide = () => {
-		if (infinte) {
-			setCurrent(current === 0 ? length - 1 : current - 1);
-		} else {
-			if (current === 0) {
-				setPrevDisabled(true);
-			} else {
-				setCurrent(current - 1);
-			}
-		}
-	};
+  const prevSlide = () => {
+    if(infinte){
+      setCurrent(current === 0 ? length - 1 : current - 1);
+    } else {
+      if(current ===  0){
+        setPrevDisabled(true)
+      } else {
+        setCurrent(current - 1)
+      }
+    }
+  };
 
-	return (
-		<div className="wrapper">
-			<FaChevronLeft
-				className="leftArrow"
-				onClick={prevSlide}
-				style={{ display: slides.length > 1 ? "inline" : "none" }}
-			/>
-			<FaChevronRight
-				className="rightArrow"
-				onClick={nextSlide}
-				style={{ display: slides.length > 1 ? "inline" : "none" }}
-			/>
-			{slides.map((slide, index) => {
-				return (
-					<div key={index}>
-						{index === current && <Slide slide={slide} alt="" />}
-					</div>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="wrapper">
+      <FaChevronLeft className="leftArrow" onClick={prevSlide} style={{display: slides.length > 1 ? "inline": "none"}}/>
+      <FaChevronRight className="rightArrow" onClick={nextSlide} style={{display: slides.length > 1  ? "inline": "none"}}/>
+      {slides.map((slide, index) => {
+        return (
+          <div key={index}>
+            {index === current && <Slide slide={slide} alt="" />}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Carousel;
